8b4ec5a20586b52e598d93e6f2e991a84cd58977
ngduczz <ndduc202@gmail.com>
2025-04-19 22:59:28 +0700

    UP

--- database/migrations/2025_02_22_153722_create_order_details_table.php
+++ database/migrations/2025_02_22_153722_create_order_details_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('variant_id');
             $table->timestamps();
 
+            $table->foreign('variant_id')->references('id')->on('product_variants')->onDelete('cascade');
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
