8f6d4a78bce2f7189a6c78372d16cc75f637dc05
ngduczz <ndduc202@gmail.com>
2025-05-16 21:09:03 +0700

    FixDetailPro

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,16 +7,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -24,27 +18,23 @@ class User extends Authenticatable
         'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
     public function payments()
-{
-    return $this->hasMany(Payment::class, 'user_id');
-}
+    {
+        return $this->hasMany(Payment::class, 'user_id');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Orders::class);
+    }
 }
